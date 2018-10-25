b2ec6b5c2cc7222841d9a93d63a85ed0aee5cdfe
Bayke <cdgl1996@gmail.com>
2018-10-25 10:15:44 -0400

    Bookstore website with 4k support
    Bookstore website

--- Telecon/Telecon/App_Start/RouteConfig.cs
+++ Telecon/Telecon/App_Start/RouteConfig.cs
@@ -22,7 +22,7 @@ namespace Telecon
             routes.MapRoute(
                 name: "Inicio",
                 url: "es/inicio",
-                defaults: new { controller = "Home", action = "Principal"}
+                defaults: new { controller = "Home", action = "Principal" }
             );
 
             routes.MapRoute(
@@ -41,115 +41,147 @@ namespace Telecon
               name: "PanelAdmin",
               url: "es/admin",
               defaults: new { controller = "Home", action = "PAdminEsp" }
-          );
+            );
 
-           routes.MapRoute(
+            routes.MapRoute(
               name: "VisualizarUsuarios",
               url: "es/admin/usuarios",
               defaults: new { controller = "Users", action = "Visualizar" }
-          );
+            );
 
-          routes.MapRoute(
+            routes.MapRoute(
              name: "EditarUsuario",
              url: "es/admin/usuarios/editarusuario",
              defaults: new { controller = "Users", action = "Editar" }
-         );
+            );
 
-          routes.MapRoute(
+            routes.MapRoute(
             name: "EliminarUsuario",
             url: "es/admin/usuarios/eliminarusuario",
             defaults: new { controller = "Users", action = "Eliminar" }
-        );
+            );
 
-          routes.MapRoute(
-         name: "InformacionContacto",
-         url: "es/contacto",
-         defaults: new { controller = "Home", action = "Contacto" }
-         );
+            routes.MapRoute(
+            name: "InformacionContacto",
+             url: "es/contacto",
+            defaults: new { controller = "Home", action = "Contacto" }
+            );
 
-         routes.MapRoute(
-         name: "InformacionGeneral",
-         url: "es/info",
-         defaults: new { controller = "Home", action = "General" }
-         );
+            routes.MapRoute(
+            name: "InformacionGeneral",
+            url: "es/info",
+            defaults: new { controller = "Home", action = "General" }
+            );
 
-       routes.MapRoute(
-       name: "Servicios",
-       url: "es/servicios",
-       defaults: new { controller = "Home", action = "Servicios" }
-       );
+            routes.MapRoute(
+            name: "Servicios",
+            url: "es/servicios",
+            defaults: new { controller = "Home", action = "Servicios" }
+            );
 
-       routes.MapRoute(
-       name: "AdminProductos",
-       url: "es/admin/productos",
-       defaults: new { controller = "Products", action = "Administrar" }
-       );
+            routes.MapRoute(
+            name: "ProductosPrincipal",
+            url: "es/productos",
+            defaults: new { controller = "Products", action = "Main" }
+            );
 
-       routes.MapRoute(
-       name: "Privilegios",
-       url: "es/admin/privilegios",
-       defaults: new { controller = "Home", action = "Privilegios" }
-       );
+            routes.MapRoute(
+            name: "AgregarProducto",
+            url: "es/productos/agregar",
+            defaults: new { controller = "Products", action = "Agregar" }
+            );
 
-       routes.MapRoute(
-       name: "Cercos",
-       url: "es/cercoselectricos",
-       defaults: new { controller = "Home", action = "Cercos" }
-       );
+            routes.MapRoute(
+            name: "ModificarProducto",
+            url: "es/productos/modificar",
+            defaults: new { controller = "Products", action = "Modificar" }
+            );
 
-     routes.MapRoute(
-     name: "Acceso",
-     url: "es/accesoautorizado",
-     defaults: new { controller = "Home", action = "EquiposAcceso" }
-     );
+            routes.MapRoute(
+            name: "EliminarProducto",
+            url: "es/productos/eliminar",
+            defaults: new { controller = "Products", action = "Eliminar" }
+            );
 
-    routes.MapRoute(
-    name: "Portones",
-    url: "es/portones",
-    defaults: new { controller = "Home", action = "Portones" }
-    );
+            routes.MapRoute(
+            name: "DetallesProducto",
+            url: "es/producto",
+            defaults: new { controller = "Products", action = "Producto" }
+            );
 
-    routes.MapRoute(
-    name: "Centrales",
-    url: "es/centralestelefonicas",
-    defaults: new { controller = "Home", action = "centrales" }
-    );
+            routes.MapRoute(
+            name: "AdminProductos",
+            url: "es/admin/productos",
+            defaults: new { controller = "Products", action = "Administrar" }
+            );
 
-    routes.MapRoute(
-    name: "OlvidarContraseña",
-    url: "es/recuperarcontraseña",
-    defaults: new { controller = "Users", action = "Olvidar" }
-    );
+            routes.MapRoute(
+            name: "Privilegios",
+            url: "es/admin/privilegios",
+            defaults: new { controller = "Home", action = "Privilegios" }
+            );
 
-    routes.MapRoute(
-    name: "MiPerfil",
-    url: "es/perfil",
-    defaults: new { controller = "Users", action = "Perfil" }
-    );
+            routes.MapRoute(
+            name: "Cercos",
+            url: "es/cercoselectricos",
+            defaults: new { controller = "Home", action = "Cercos" }
+            );
 
-    routes.MapRoute(
-    name: "ActualizarPerfil",
-    url: "es/perfil/actualizarperfil",
-    defaults: new { controller = "Users", action = "ActualizarPerfil" }
-    );
+            routes.MapRoute(
+            name: "Acceso",
+            url: "es/accesoautorizado",
+            defaults: new { controller = "Home", action = "EquiposAcceso" }
+            );
 
-    routes.MapRoute(
-    name: "CambiarContraseña",
-    url: "es/perfil/cambiarcontraseña",
-    defaults: new { controller = "Users", action = "CambiarContraseña" }
-    );
+            routes.MapRoute(
+            name: "Portones",
+            url: "es/portones",
+            defaults: new { controller = "Home", action = "Portones" }
+            );
 
-    routes.MapRoute(
-    name: "MisClientes",
-    url: "es/perfil/clientes",
-    defaults: new { controller = "Users", action = "MisClientes" }
-    );
+            routes.MapRoute(
+            name: "Centrales",
+            url: "es/centralestelefonicas",
+            defaults: new { controller = "Home", action = "centrales" }
+            );
 
-           routes.MapRoute(
-           name: "Chat",
-           url: "es/chat",
-           defaults: new { controller = "Users", action = "Chat" }
-           );
+            routes.MapRoute(
+            name: "OlvidarContraseña",
+            url: "es/recuperarcontraseña",
+            defaults: new { controller = "Users", action = "Olvidar" }
+            );
+
+            routes.MapRoute(
+            name: "MiPerfil",
+            url: "es/perfil",
+            defaults: new { controller = "Users", action = "Perfil" }
+            );
+
+            routes.MapRoute(
+            name: "ActualizarPerfil",
+            url: "es/perfil/actualizarperfil",
+            defaults: new { controller = "Users", action = "ActualizarPerfil" }
+            );
+
+            routes.MapRoute(
+            name: "CambiarContraseña",
+            url: "es/perfil/cambiarcontraseña",
+            defaults: new { controller = "Users", action = "CambiarContraseña" }
+            );
+
+            routes.MapRoute(
+            name: "MisClientes",
+            url: "es/perfil/clientes",
+            defaults: new { controller = "Users", action = "MisClientes" }
+            );
+
+            routes.MapRoute(
+            name: "Chat",
+            url: "es/chat",
+            defaults: new { controller = "Users", action = "Chat" }
+            );
+
+
 
 
             /* ENGLISH VERSION */
@@ -166,43 +198,43 @@ namespace Telecon
                  defaults: new { controller = "Home", action = "UserLogin" }
              );
 
-            routes.MapRoute(
+                routes.MapRoute(
               name: "CreateUser",
               url: "en/admin/users/createuser",
               defaults: new { controller = "Users", action = "Create" }
-          );
+            );
 
             routes.MapRoute(
               name: "PAdmin",
               url: "en/admin",
               defaults: new { controller = "Home", action = "PAdminEng" }
-          );
+            );
 
             routes.MapRoute(
               name: "Users",
               url: "en/admin/users",
               defaults: new { controller = "Users", action = "ViewUsers" }
-          );
+            );
 
 
             routes.MapRoute(
             name: "EditUser",
             url: "en/admin/users/edituser",
             defaults: new { controller = "Users", action = "Edit" }
-        );
+            );
 
             routes.MapRoute(
               name: "DeleteUser",
               url: "en/admin/users/deleteuser",
               defaults: new { controller = "Users", action = "Delete" }
-          );
+            );
 
 
             routes.MapRoute(
-       name: "ContactInfo",
-       url: "en/contact",
-       defaults: new { controller = "Home", action = "Contact" }
-       );
+            name: "ContactInfo",
+            url: "en/contact",
+            defaults: new { controller = "Home", action = "Contact" }
+            );
 
             routes.MapRoute(
             name: "GeneralInfo",
@@ -217,6 +249,36 @@ namespace Telecon
             );
 
             routes.MapRoute(
+            name: "Products",
+            url: "en/products",
+            defaults: new { controller = "Products", action = "MainPage" }
+            );
+
+            routes.MapRoute(
+            name: "AddProduct",
+            url: "en/products/add",
+            defaults: new { controller = "Products", action = "Add" }
+            );
+
+            routes.MapRoute(
+            name: "EditProduct",
+            url: "en/products/edit",
+            defaults: new { controller = "Products", action = "Edit" }
+            );
+
+            routes.MapRoute(
+            name: "DeleteProduct",
+            url: "en/products/delete",
+            defaults: new { controller = "Products", action = "Delete" }
+            );
+
+            routes.MapRoute(
+           name: "ProductDetails",
+           url: "en/product",
+           defaults: new { controller = "Products", action = "Product" }
+           );
+
+            routes.MapRoute(
             name: "ProductsAdmin",
             url: "en/admin/products",
             defaults: new { controller = "Products", action = "Admin" }
@@ -229,10 +291,10 @@ namespace Telecon
             );
 
             routes.MapRoute(
-      name: "ElectricFences",
-      url: "en/electricfences",
-      defaults: new { controller = "Home", action = "ElectricFences" }
-      );
+            name: "ElectricFences",
+            url: "en/electricfences",
+            defaults: new { controller = "Home", action = "ElectricFences" }
+            );
 
             routes.MapRoute(
             name: "CCTV",
--- Telecon/Telecon/Controllers/ProductsController.cs
+++ Telecon/Telecon/Controllers/ProductsController.cs
@@ -17,5 +17,48 @@ namespace Telecon.Controllers
         {
             return View("AdminProducts");
         }
+        public ActionResult Main()
+        {
+            return View("Productos");
+        }
+        public ActionResult MainPage()
+        {
+            return View("Products");
+        }
+
+        public ActionResult Agregar()
+        {
+            return View("NuevoProducto");
+        }
+
+        public ActionResult Modificar()
+        {
+            return View("ModificarProducto");
+        }
+
+        public ActionResult Eliminar()
+        {
+            return View("EliminarProducto");
+        }
+        public ActionResult Producto()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View("AddProduct");
+        }
+        public ActionResult Edit()
+        {
+            return View("EditProduct");
+        }
+        public ActionResult Delete()
+        {
+            return View("DeleteProduct");
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
     }
 }
